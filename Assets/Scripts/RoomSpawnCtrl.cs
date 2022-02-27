3edebe853a86757c0ac1727f7f96eedce4105474
Maykel S Braz <maykelsb@yahoo.com.br>
2022-02-26 23:39:31 -0300

    Codding Room Spawner

--- Assets/Scripts/RoomSpawnCtrl.cs
+++ Assets/Scripts/RoomSpawnCtrl.cs
@@ -7,17 +7,44 @@ public class RoomSpawnCtrl : MonoBehaviour
     public enum Doors { top, right, bottom, left };
     public Doors doorNeeded;
 
-    void Update()
+    private RoomTemplates templates;
+    private int randomRoomIndx;
+    private GameObject[] roomList;
+    //private bool spawned = false;
+
+    void Start()
+    {
+        templates = GameObject
+            .FindGameObjectWithTag("Rooms")
+            .GetComponent<RoomTemplates>();
+        Invoke("SpawnRoom", 5.0f);
+    }
+
+    void SpawnRoom()
     {
         switch (doorNeeded) {
-            case Doors.top:
-                break;
-            case Doors.right:
-                break;
-            case Doors.bottom:
-                break;
-            case Doors.left:
-                break;
+            case Doors.top: roomList = templates.topRooms; break;
+            case Doors.right: roomList = templates.rightRooms; break;
+            case Doors.bottom: roomList = templates.bottomRooms; break;
+            case Doors.left: roomList = templates.leftRooms; break;
+        }
+        randomRoomIndx = Random.Range(0, roomList.Length);
+        Instantiate(
+            roomList[randomRoomIndx],
+            transform.position,
+            roomList[randomRoomIndx].transform.rotation
+        );
+        //spawned = true;
+        CancelInvoke("SpawnRoom");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("colidou");
+        if (other.CompareTag("SpawnPoint")/* && other.GetComponent<RoomSpawnCtrl>().spawned*/)
+        {
+            Debug.Log("here");
+            Destroy(gameObject);
         }
     }
 }
---
+++ Assets/Scripts/RoomTemplates.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+    public GameObject[] topRooms;
+    public GameObject[] rightRooms;
+    public GameObject[] bottomRooms;
+    public GameObject[] leftRooms;
+}
