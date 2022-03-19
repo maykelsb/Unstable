15d30dc090648d8b2dc3d053fe789aabd441e8be
Maykel Santos Braz <maykelsb@yahoo.com.br>
2022-03-19 09:57:13 -0300

    Adjusting last internal wall inserted and adding a new one

--- Assets/Scripts/Rooms/InteriorWall.cs
+++ Assets/Scripts/Rooms/InteriorWall.cs
@@ -15,9 +15,10 @@ public class InteriorWall : MonoBehaviour
     private bool flipY;
 
     [SerializeField]
-    private int[] allowedX; // -- displace of 9 units (3/-6)
+    private float[] allowedX; // -- displace of 9 units (3/-6) on flip
+
     [SerializeField]
-    private int[] allowedY;
+    private float[] allowedY;
 
     private bool flippedX = false;
     private bool flippedY = false;
@@ -55,5 +56,94 @@ public class InteriorWall : MonoBehaviour
     public bool IsFlippedY()
     {
         return flippedY;
+    }
+
+    public InteriorWall SetWall(GameObject room)
+    {
+        return SetParent(room)
+            .FlipX()
+            .SetPosition();
+    }
+
+    private InteriorWall SetParent(GameObject room)
+    {
+        transform.SetParent(room.transform);
+        return this;
+    }
+
+    private InteriorWall FlipX()
+    {
+        if (!CanFlipX())
+        {
+            return this;
+        }
+
+
+        int newX = ((Random.Range(0, 2) * 2) - 1);
+        if (newX < 0)
+        {
+            flippedX = true;
+            transform.localScale = new Vector3(newX, 1f, 1f);
+        }
+
+        return this;
+    }
+
+    private InteriorWall SetPosition()
+    {
+        transform.localPosition = new Vector3(
+            GetXPos(),
+            GetYPos()
+        );
+
+        return this;
+    }
+
+
+
+
+
+
+    private float GetXPos()
+    {
+        float newX = this.allowedX[Random.Range(0, this.allowedX.Length)];
+        if (this.flippedX)
+            return newX + 9;
+
+        return newX;
+    }
+
+    private float GetYPos()
+    {
+        float newY = this.allowedY[Random.Range(0, this.allowedY.Length)];
+        //if (this.flippedX)
+        //    return newX + 9;
+
+        return newY;
+    }
+
+    public InteriorWall PositionY()
+    {
+
+        // -6 / -4.5f | 2 / 1.5f
+        // 3 / 4.5f | 2 / 1.5f
+
+
+
+
+        //        Vector3 roomPos = room.transform.GetChild(0).transform.position;
+        //        Vector3 newPos = new Vector3(roomPos.x - 8, roomPos.y + 1);
+        //        Vector3 newPos = new Vector3(roomPos.x - 8, roomPos.y + 1);
+
+        //interiorWall.transform.SetParent(room.transform);
+        //interiorWall.transform.localPosition = new Vector3(-4.5f, 1.5f);
+        //interiorWall.transform.localScale = new Vector3(-1.0f, 1f, 1f);
+        //interiorWall.transform.localPosition = new Vector3(4.5f, 1.5f);
+
+
+
+
+
+        return this;
     }
 }
